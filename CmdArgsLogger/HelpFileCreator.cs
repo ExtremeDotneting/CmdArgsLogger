d7a52a989299648768e583f0f816ab0ccec916da
gfys <yuram1inbox@gmail.com>
2022-08-21 18:10:12 +0300

    Initial

---
+++ CmdArgsLogger/HelpFileCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdArgsLogger
+{
+    internal static class HelpFileCreator
+    {
+        public static string CreateAndOpenHelpFile(string exeFile, string jsonFileThatNotFound)
+        {
+            var text= $"--- ERROR ---\nConfiguration file '{jsonFileThatNotFound}' for cmd logger was not found."+
+                @"
+
+--- INFO ---
+"
+        }
+    }
+}
---
+++ CmdArgsLogger/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CmdArgsLogger
+{
+    class Program
+    {
+        private static string _currentExeFullPath;
+        private static string _argsString;
+        private static Process _currentProcess;
+
+        public static void Main(string[] args)
+        {
+            _currentProcess = Process.GetCurrentProcess();
+            _currentExeFullPath = _currentProcess.MainModule.FileName;
+            _argsString = string.Join(' ', args);
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName=
+            });
+           
+            Console.WriteLine(argsString);
+            Console.ReadLine();
+        }
+
+        RunOptions TryGetRunOptions()
+        {
+            var jsonPath = _currentExeFullPath.Remove(_currentExeFullPath.Length - ".exe".Length) + "cmd_args_logger.json";
+            if (File.Exists(jsonPath))
+            {
+
+            }
+            var jsonFile = Process.GetCurrentProcess();
+        }
+    }
+}
